fdbc79fa80f99ca46883123a8f4c0f03dc95e1e6
ShotaEbikawa <shotaebikawa@gmail.com>
2019-04-17 16:33:00 -0700

    fixed the bug in crossValidate function

--- python/project-files/machine_learning.py
+++ python/project-files/machine_learning.py
@@ -27,7 +27,7 @@ from sklearn import svm
 from sklearn.model_selection import KFold,cross_val_score
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-
+from sklearn.metrics import confusion_matrix
 
 
 
@@ -39,6 +39,7 @@ def preprocessing(X):
                     '..', '--', '---',':','~','=','`','{','}','\n']
     # new_X(which is a list) stores real_token
     new_X = []
+    y = ''
     lemmatize = WordNetLemmatizer()
     stemmer = SnowballStemmer("english")
     # iterates through a column containing text reviews
@@ -50,11 +51,17 @@ def preprocessing(X):
         tokens = nltk.word_tokenize(str(review))
         # iterate through the tokenized text review
         for t in tokens:
+            y = ''
+            for ch in t:
+                if ch in stop_words or ch in punctuation:
+                    continue
+                else:
+                    y += ch;
             # real_token appends t
             # ONLY IF t is not a stop word
-            if t not in stop_words and t not in punctuation:
-                t = stemmer.stem(t)
-                t = lemmatize.lemmatize(t)
+            if y not in stop_words and y not in punctuation:
+                #t = stemmer.stem(t)
+                t = lemmatize.lemmatize(y)
                 real_token.append(t.lower())
         # the text review will be reinitialized to real_token
         review = real_token
@@ -114,65 +121,21 @@ def classifier(classifier_name, X_train, Y_train):
         
 def crossValidate(classifier, X, Y):
     k_fold = KFold(n_splits=10, shuffle= True)
-    if (classfier == 'LinearSVC'):
+    if (classifier == 'LinearSVC'):
         clf = svm.LinearSVC(multi_class='ovr')
-        cross_v_tdidf = cross_val_score(clf, X_df, Y, cv=k_fold, n_jobs=1)
-        cross_v_bow = cross_val_score(multinomialNB, BOW_df, Y, cv=k_fold, n_jobs=1)
+        cross_v_tdidf = cross_val_score(clf, X, Y, cv=k_fold, n_jobs=1)
+#        cross_v_bow = cross_val_score(multinomialNB, BOW_df, Y, cv=k_fold, n_jobs=1)
         num = 0
         for i in cross_v_tdidf:
             num +=i
         print(num/10)
         
         num = 0
-        for i in cross_v_bow:
-            num +=i
-        print(num/10)
-
+#        for i in cross_v_bow:
+#            num +=i
+#        print(num/10)
+        return cross_v_tdidf
     
-def produceSentiment(column_name, test_Y):
-    file_name = pd.read_json(r"C:\Users\shota\Desktop\Yelp-Sentiment-Analysis\python\project-files\src\dummy-review.json")
-    i = 0
-    j = 1
-    k = 2
-    pos_count = 0
-    neu_count = 0
-    neg_count = 0
-    test_Y_list = []
-    while (i < len(test_Y)):
-        if (test_Y[i] and test_Y[i] == 'positive'):
-            pos_count += 1
-        elif (test_Y[i] and test_Y[i] == 'neutral'):
-            neu_count += 1
-        else:
-            neg_count += 1
-        if (test_Y[j] and test_Y[j] == 'positive'):
-            pos_count += 1
-        elif (test_Y[j] and test_Y[j] == 'neutral'):
-            neu_count += 1
-        else:
-            neg_count += 1
-        if (test_Y[k] and test_Y[k] == 'positive'):
-            pos_count += 1
-        elif (test_Y[k] and test_Y[k] == 'neutral'):
-            neu_count += 1
-        else:
-            neg_count += 1
-            
-        cmax = max(pos_count, neu_count, neg_count)
-        if cmax == pos_count:
-            test_Y_list.append('positive')
-        elif cmax == neu_count:
-            test_Y_list.append('neutral')
-        else:
-            test_Y_list.append('negative')
-        i += 3
-        j += 3
-        k += 3
-        pos_count = 0
-        neu_count = 0
-        neg_count = 0
-    data[column_name] = test_Y_list
-# imports csv file of restaurant-review-labeled-data.csv
     
 def labelEncoder(Y):
     encoder = preprocessing.LabelEncoder()
@@ -180,6 +143,43 @@ def labelEncoder(Y):
     return Y
 
 
+def evaluateCM(y_pred, Y_test):
+    cm = confusion_matrix(y_pred, Y_test)
+    t_pos_neg = cm[0][0]
+    t_neg_neg = cm[1][1]+cm[1][2]+cm[2][1]+cm[2][2]
+    f_pos_neg = cm[1][0]+cm[2][0]
+    f_neg_neg = cm[0][1]+cm[0][2]
+    
+    
+    t_pos_neu = cm[1][1]
+    t_neg_neu = cm[0][0]+cm[0][2]+cm[2][0]+cm[2][2]
+    f_pos_neu = cm[0][1]+cm[2][1]
+    f_neg_neu = cm[1][0]+cm[1][2]
+    
+    t_pos_pos = cm[2][2]
+    t_neg_pos = cm[0][0]+cm[0][1]+cm[1][0]+cm[1][1]
+    f_pos_pos = cm[0][2]+cm[1][2]
+    f_neg_pos = cm[2][0]+cm[2][1]
+    
+    accuracy = (t_pos_neg + t_pos_neu + t_pos_pos) / (t_pos_neg + t_pos_neu + t_pos_pos + f_neg_neg + f_neg_neu + f_neg_pos)
+    precision_pos = t_pos_pos / (t_pos_pos + f_pos_pos)
+    precision_neg = t_pos_neg / (t_pos_neg + f_pos_neg)
+    precision_neu = t_pos_neu / (t_pos_neu + f_pos_neu)
+    
+    recall_pos = t_pos_pos / (t_pos_pos + f_neg_pos)
+    recall_neg = t_pos_neg / (t_pos_neg + f_neg_neg)
+    recall_neu = t_pos_neu / (t_pos_neu + f_neg_neu)
+    
+    avg_precision = (precision_pos + precision_neg + precision_neu) / 3
+    avg_recall = (recall_pos + recall_neg + recall_neu) / 3
+    
+    f1 = (2 * avg_precision * avg_recall) / (avg_precision + avg_recall)
+    
+    print("The accuracy is: ", accuracy*100, "%")
+    print("The precision is: ", avg_precision*100, "%")
+    print("The recall is: ", avg_recall*100, "%")
+    print("The f1 score is: ", f1*100, "%")
+    
 
 
 
@@ -204,16 +204,18 @@ Y = restaurant_labeled.iloc[1:, 11:12]
 
 X = preprocessing(X)
 X = featuring(X, 'TFIDF')
+crossValidate = crossValidate('LinearSVC', X, Y)
 #X_train, X_test, Y_train, Y_test = train_test_split(BOW_df, Y, test_size = 0.25, random_state = 0)
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.25, random_state = 0)
 clf = classifier('LinearSVC', X_train, Y_train)
 y_pred = list(clf.predict(X_test))
 printAccuracy(y_pred,Y_test)
+evaluateCM(y_pred, Y_test)
 
-test_data = pd.read_csv('src/restaurant-review.csv')
-test_X = test_data.iloc[:,-1]
-test_X = preprocessing(test_X)
-test_X = featuring(test_X, 'TFIDF')
-test_Y = list(clf.predict(test_X))
-produceSentiment('overall_senti', test_Y)
+#test_data = pd.read_csv('src/restaurant-review.csv')
+#test_X = test_data.iloc[:,-1]
+#test_X = preprocessing(test_X)
+#test_X = featuring(test_X, 'TFIDF')
+#test_Y = list(clf.predict(test_X))
+#produceSentiment('overall_senti', test_Y)
 

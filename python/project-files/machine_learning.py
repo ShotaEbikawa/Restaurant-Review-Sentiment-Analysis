50548d5839ff4667912b70da736555fc7f70b5e8
tasogrigoriou <taso.grigoriou@gmail.com>
2019-05-13 11:09:40 -0700

    Added different classifiers and put them into their own pipeline, compare the results with a graph plot

--- python/project-files/machine_learning.py
+++ python/project-files/machine_learning.py
@@ -34,9 +34,16 @@ from sklearn.pipeline import Pipeline, FeatureUnion
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
 from Extractors import ColumnSelector, PositiveWordCountExtractor, NegativeWordCountExtractor, UppercaseWordCountExtractor, AverageWordLengthExtractor, ExclamationPointCountExtractor, UsefulValueExtractor, ClfSwitcher
+from matplotlib.pyplot import figure
 
 lemmatizer = WordNetLemmatizer()
 
+decoder = {1: 'negative', 3: 'neutral', 5: 'positive'}
+
+num_samples = 100
+
+classifier_list = ['SVC', 'MNB', 'RFC', 'LR']
+accuracy_list = []
 
 def preprocessing(X):
     real_token = []
@@ -194,86 +201,6 @@ def add_vote_columns(yelp_df):
     return yelp_df
 
 
-def setup_pipeline():
-    positive_words = [pos_word.rstrip('\n') for pos_word in open('src/positive-words.txt')]
-    negative_words = [neg_word.rstrip('\n') for neg_word in open('src/negative-words.txt')]
-    
-    # Features for pipeline
-    tfidf = TfidfVectorizer(ngram_range=(1, 2), stop_words='english',
-                            min_df=0., max_df=1.)
-    
-    pos_word_count = PositiveWordCountExtractor(positive_words)
-    neg_word_count = NegativeWordCountExtractor(negative_words)
-    uppercase_word_count = UppercaseWordCountExtractor()
-    avg_word_length = AverageWordLengthExtractor()
-    exclamation_count = ExclamationPointCountExtractor()
-    
-    useful_value = UsefulValueExtractor()
-
-    # Classifiers for pipeline
-    svc = svm.LinearSVC(multi_class='crammer_singer')
-
-    # Pipeline that selects the text column and extracts tfidf score of terms with TfidfVectorizer
-    tfidf_pipeline = Pipeline([
-        ('selector', ColumnSelector(key='text')),
-        ('tfidf', tfidf)
-    ])
-        
-    # Pipeline that selects the text column and computes the number of 'positive' words in a review text
-    pos_word_count_pipeline = Pipeline([
-        ('selector', ColumnSelector(key='text')),
-        ('pos-word', pos_word_count)
-    ])
-
-    # Pipeline that selects the text column and computes the number of 'negative' words in a review text
-    neg_word_count_pipeline = Pipeline([
-        ('selector', ColumnSelector(key='text')),
-        ('neg-word', neg_word_count)
-    ])
-        
-    # Pipeline that selects the text column and computes the number of uppercased words for each text review
-    uppercase_word_count_pipeline = Pipeline([
-        ('selector', ColumnSelector(key='text')),
-        ('uppercase-word', uppercase_word_count)
-    ])
-
-    # Pipeline that selects the text column and computes average word length for each text review
-    avg_word_length_pipeline = Pipeline([
-        ('selector', ColumnSelector(key='text')),
-        ('ave', avg_word_length)
-    ])
-
-    # Pipeline that selects the text column and extracts the number of exclamation points for each text review
-    exclamation_pipeline = Pipeline([
-        ('selector', ColumnSelector(key='text')),
-        ('exclamation', exclamation_count)
-    ])
-
-    # Pipeline that selects the votes column and extracts the value at the 'useful' key
-    useful_value_pipeline = Pipeline([
-        ('selector', ColumnSelector(key='useful')),
-        ('useful-value', useful_value)
-    ])
-
-    # FeatureUnion all individual feature pipelines
-    feature_pipeline = FeatureUnion([
-        ('tfidf', tfidf_pipeline),
-        ('pos-word-count', pos_word_count_pipeline),
-        ('neg-word-count', neg_word_count_pipeline),
-        ('uppercase-word-count', uppercase_word_count_pipeline),
-        ('avg-word-length', avg_word_length_pipeline),
-        ('exclamation', exclamation_pipeline),
-        ('useful-value', useful_value_pipeline)
-    ])
-
-    pipeline = Pipeline([
-        ('features', feature_pipeline),
-        ('classifier', svc)
-    ])
-
-    return pipeline
-
-
 def get_majority_classifier(Y):
     classifiers = {}
 
@@ -312,6 +239,80 @@ def get_class_distribution(Y):
     return class_dist
 
 
+def setup_feature_pipeline():
+    # Open pos/neg words text file and store the words into a list
+    positive_words = [pos_word.rstrip('\n') for pos_word in open('src/positive-words.txt')]
+    negative_words = [neg_word.rstrip('\n') for neg_word in open('src/negative-words.txt')]
+
+    # Features for pipeline
+    tfidf = TfidfVectorizer(ngram_range=(1, 2), stop_words='english',
+                            min_df=0., max_df=1.)
+
+    pos_word_count = PositiveWordCountExtractor(positive_words)
+    neg_word_count = NegativeWordCountExtractor(negative_words)
+
+    uppercase_word_count = UppercaseWordCountExtractor()
+    avg_word_length = AverageWordLengthExtractor()
+    exclamation_count = ExclamationPointCountExtractor()
+
+    useful_value = UsefulValueExtractor()
+
+    # Pipeline that selects the text column and extracts tfidf score of terms with TfidfVectorizer
+    tfidf_pipeline = Pipeline([
+        ('selector', ColumnSelector(key='text')),
+        ('tfidf', tfidf)
+    ])
+
+    # Pipeline that selects the text column and computes the number of 'positive' words in a review text
+    pos_word_count_pipeline = Pipeline([
+        ('selector', ColumnSelector(key='text')),
+        ('pos-word', pos_word_count)
+    ])
+
+    # Pipeline that selects the text column and computes the number of 'negative' words in a review text
+    neg_word_count_pipeline = Pipeline([
+        ('selector', ColumnSelector(key='text')),
+        ('neg-word', neg_word_count)
+    ])
+
+    # Pipeline that selects the text column and computes the number of uppercased words for each text review
+    uppercase_word_count_pipeline = Pipeline([
+        ('selector', ColumnSelector(key='text')),
+        ('uppercase-word', uppercase_word_count)
+    ])
+
+    # Pipeline that selects the text column and computes average word length for each text review
+    avg_word_length_pipeline = Pipeline([
+        ('selector', ColumnSelector(key='text')),
+        ('ave', avg_word_length)
+    ])
+
+    # Pipeline that selects the text column and extracts the number of exclamation points for each text review
+    exclamation_pipeline = Pipeline([
+        ('selector', ColumnSelector(key='text')),
+        ('exclamation', exclamation_count)
+    ])
+
+    # Pipeline that selects the votes column and extracts the value at the 'useful' key
+    useful_value_pipeline = Pipeline([
+        ('selector', ColumnSelector(key='useful')),
+        ('useful-value', useful_value)
+    ])
+
+    # FeatureUnion all individual feature pipelines
+    feature_pipeline = FeatureUnion([
+        ('tfidf', tfidf_pipeline),
+        ('pos-word-count', pos_word_count_pipeline),
+        ('neg-word-count', neg_word_count_pipeline),
+        ('uppercase-word-count', uppercase_word_count_pipeline),
+        ('avg-word-length', avg_word_length_pipeline),
+        ('exclamation', exclamation_pipeline),
+        ('useful-value', useful_value_pipeline)
+    ])
+
+    return feature_pipeline
+
+
 def k_fold_cross_validation(k, pipeline, X_train, Y_train):
     scores = cross_val_score(
         pipeline,
@@ -325,21 +326,45 @@ def k_fold_cross_validation(k, pipeline, X_train, Y_train):
     return scores
 
 
-def evaluate(y_pred, Y_test, X_test):
-    print('Accuracy:\n', accuracy_score(Y_test, y_pred))
-    print('Confusion Matrix:\n', confusion_matrix(Y_test, y_pred))
-    print(classification_report(Y_test, y_pred))
+def make_prediction(X, Y, pipeline, classifier_name):
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25, random_state=0)
 
-    encoder = {1: 'negative', 3: 'neutral', 5: 'positive'}
-    
+    # Instead of fitting the pipeline on the training data, 
+    # do cross-validation too so that you know if it's overfitting.
+    # This returns an array of values, each having the score for an individual run.
+    # k=10 implies 10-fold cross validation
+    #scores = k_fold_cross_validation(10, pipeline, X_train, Y_train)
+
+    pipeline.fit(X_train, Y_train.values.ravel())
+
+    y_pred = pipeline.predict(X_test)
+
+    evaluate(y_pred, Y_test, X_test, classifier_name)
+
+
+def evaluate(y_pred, Y_test, X_test, classifier_name):
+    print(f'\nEvaluation results for classifier: {classifier_name}\n')
+    print('Accuracy:\n', accuracy_score(Y_test, y_pred))
+    accuracy_list.append(accuracy_score(Y_test, y_pred))
+    print('Confusion Matrix:\n', confusion_matrix(Y_test, y_pred))
+    print('Classification report:\n', classification_report(Y_test, y_pred))
+
     # Iterate through X_test total_iters times, printing the review text and predicted output label
     total_iters = 10
     y_index = 0
+    print(f'Showing {total_iters} results of review text and predicted output label...\n')
     for index, row in X_test.iterrows():
-        print(f'Review text: \n{row["text"]} \n\nPredicted: \n\n{encoder[y_pred[y_index]]}\n')
+        print(f'Review text: \n{row["text"]} \n\nPredicted: \n{decoder[y_pred[y_index]]}\n')
         y_index += 1
         if y_index >= total_iters:
             break
+        
+        
+def plot_graph():
+    figure(num=None, figsize=(12, 6))
+    plt.scatter(classifier_list, accuracy_list)
+    plt.xlabel('Classifiers')
+    plt.ylabel('Accuracy')
 
 
 data = pd.read_json('src/restaurant-review.json')
@@ -356,8 +381,6 @@ yelp_data1 = yelp_data[(yelp_data['stars'] == 1)]
 yelp_data3 = yelp_data[(yelp_data['stars'] == 3)]
 yelp_data5 = yelp_data[(yelp_data['stars'] == 5)]
 
-num_samples = 100
-
 # Take all 1 star, 3 star, and 5 star rows for both text and useful columns to use as X text/train data
 X = pd.concat([
     yelp_data1.loc[:, ['text', 'useful']].sample(n=num_samples),
@@ -372,24 +395,33 @@ Y = pd.concat([
     yelp_data5.loc[:, ['stars']].sample(n=num_samples)
 ])
 
+# Get our majority classifier and class distribution and print their values
 majority_classifier = get_majority_classifier(Y)
 class_distribution = get_class_distribution(Y)
 
-pipeline = setup_pipeline()
+# Setup feature pipeline that includes all of our custom feature extractors
+feature_pipeline = setup_feature_pipeline()
 
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.25, random_state=0)
+# A dictionary of classifiers with their name as key 
+# We will use the dict to make predictions for our data and record the results for each classifier
+classifier_dict = {
+    'Support Vector Machine (SVC)': svm.LinearSVC(multi_class='crammer_singer'),
+    'Multinomial Naive Bayes': MultinomialNB(),
+    'Random Forest Classifier': RandomForestClassifier(),
+    'Logistic Regression': LogisticRegression()
+}
 
-# Instead of fitting the pipeline on the training data, 
-# do cross-validation too so that you know if it's overfitting.
-# This returns an array of values, each having the score for an individual run.
-# k=10 implies 10-fold cross validation
-#scores = k_fold_cross_validation(10, pipeline, X_train, Y_train)
+# Iterate through our classifier dict, setup a pipeline with our original features, then make predictions
+for name, classifier in classifier_dict.items():
+    pipeline = Pipeline([
+        ('features', feature_pipeline),
+        ('classifier', classifier)
+    ])
 
-pipeline.fit(X_train, Y_train.values.ravel())
-
-y_pred = pipeline.predict(X_test)
-
-evaluate(y_pred, Y_test, X_test)
+    make_prediction(X, Y, pipeline, name)
+    
+# Plot graph of classifiers and accuracy on x,y axis
+plot_graph()
 
 #test_data = pd.read_csv('src/restaurant-review.csv')
 #test_X = test_data.iloc[:,-1]
